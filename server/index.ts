a066eb8856a75d83a962f5bae6a30461a4de2e67
Greg Gambone <58961261+TwistedNonsense@users.noreply.github.com>
2025-01-30 10:09:38 -0500

    Removed dynamic imports. This now only imports server.ts

--- server/index.ts
+++ server/index.ts
@@ -2,14 +2,6 @@ console.log("Loading index.ts...");
 
 const PORT = process.env.PORT || 5000;
 
-import app from './server';
-
-async function loadRoutes() {
-    console.log("Loading API routes...");
-    const apiRoutes = (await import('./api')).default;
-    app.use('/api', apiRoutes);
-}
-
-loadRoutes();
+import app from './server'; // `server.ts` now loads API routes
 
 app.listen(PORT, () => console.log(`Backend server is running on http://localhost:${PORT}`));
