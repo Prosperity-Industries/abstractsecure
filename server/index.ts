101644d14f1c5a560595b9978e68e4dfa8cbb435
Greg Gambone <58961261+TwistedNonsense@users.noreply.github.com>
2025-01-30 12:16:59 -0500

    Created index.ts to replace server.ts

---
+++ server/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import apiRoutes from './api';
+
+const createServer = () => {
+    const app = express();
+    app.use(cors());
+    app.use('/api', apiRoutes);
+    return app;
+};
+
+export { createServer };
