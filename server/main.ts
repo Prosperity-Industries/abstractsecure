9210cdd1015561a9922699135dfa2af39c7e15ad
Greg Gambone <58961261+TwistedNonsense@users.noreply.github.com>
2025-01-30 10:59:51 -0500

    Updated to use app.ts instead of server.ts

--- server/main.ts
+++ server/main.ts
@@ -1,7 +1,11 @@
-console.log("Loading index.ts...");
+console.log("Loading main.ts...");
+
+import app from './app';
 
 const PORT = process.env.PORT || 5000;
 
-import app from './server'; // `server.ts` now loads API routes
+const startServer = () => {
+    app.listen(PORT, () => console.log(`Backend server is running on http://localhost:${PORT}`));
+};
 
-app.listen(PORT, () => console.log(`Backend server is running on http://localhost:${PORT}`));
+startServer();
