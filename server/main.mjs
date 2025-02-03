0ec245a729fdb77276bc9c14250c7dc4afae681d
Greg Gambone <58961261+TwistedNonsense@users.noreply.github.com>
2025-02-03 12:30:03 -0500

    Created mjs files to comply with Node.js ESM syntax requirements

---
+++ server/main.mjs
@@ -0,0 +1,6 @@
+console.log("Loading main.mjs...");
+import 'dotenv/config';
+import { createServer } from './index.mjs';
+const PORT = process.env.PORT || 5000;
+const app = createServer();
+app.listen(PORT, () => console.log(`Backend server is running on http://localhost:${PORT}`));
