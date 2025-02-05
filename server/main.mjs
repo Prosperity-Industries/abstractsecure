a61a3bdeb046a50f61101e15df89ea881407f1cc
Greg Gambone <58961261+TwistedNonsense@users.noreply.github.com>
2025-02-05 13:45:05 -0500

    Corrected port for Cloud Run default

--- server/main.mjs
+++ server/main.mjs
@@ -4,6 +4,6 @@ console.log("Loading main.mjs...");
 import 'dotenv/config';
 import { createServer } from './index.mjs';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080; // Google Cloud Run default
 const app = createServer();
 app.listen(PORT, () => console.log(`Backend server is running on http://localhost:${PORT}`));
